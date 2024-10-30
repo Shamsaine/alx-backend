db8bcb712a5451b1093995818574e90b772a5602
shamsudeen Ibrahim <shamsain3@gmail.com>
2024-10-30 17:14:00 +0000

    added some more file

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Least Frequently Used caching module.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a LFU
+    algorithm when the limit is reached.
+    """
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.keys_freq = []
+
+    def __reorder_items(self, mru_key):
+        """Reorders the items in this cache based on the most
+        recently used item.
+        """
+        max_positions = []
+        mru_freq = 0
+        mru_pos = 0
+        ins_pos = 0
+        for i, key_freq in enumerate(self.keys_freq):
+            if key_freq[0] == mru_key:
+                mru_freq = key_freq[1] + 1
+                mru_pos = i
+                break
+            elif len(max_positions) == 0:
+                max_positions.append(i)
+            elif key_freq[1] < self.keys_freq[max_positions[-1]][1]:
+                max_positions.append(i)
+        max_positions.reverse()
+
